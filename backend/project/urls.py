37618d0acfb25999efba39aa78f754219179b624
Kevin <malieakkalkevin@gmail.com>
2025-01-13 14:17:16 +0100

    url patterns for media added

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -23,5 +23,6 @@ from project import settings
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
 ]
+# hi
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
