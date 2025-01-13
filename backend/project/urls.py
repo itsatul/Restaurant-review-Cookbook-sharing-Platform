97a27a94e33041c22ae9608a920311a2596b1a2f
Jon_Work_Linux <jon.garmilla.work@gmail.com>
2025-01-13 18:03:51 +0100

    add some changes , docs

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -17,11 +17,28 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from rest_framework.permissions import AllowAny
 
 from project import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django REST API",
+        default_version="v1",
+        description="My nice webshop", # you can modify to your preference
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="academy@constructor.org"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny,])
 
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
+    path('backend/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.models import User
+
+class GetAllUsersView(APIView):
+
+
+    def get(self, request):
+        users = User.objects.all()
+        users_data = [user.id for user in users]
+        return Response(users_data, status=status.HTTP_200_OK)
