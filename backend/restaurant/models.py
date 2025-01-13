f774c6d09fd41d9e866ca3d665a558b778f52d8a
Kevin <malieakkalkevin@gmail.com>
2025-01-13 19:12:02 +0100

    saving work

--- backend/restaurant/models.py
+++ backend/restaurant/models.py
@@ -1,6 +1,12 @@
 # Create your models here.
+from django.contrib.auth import get_user_model
 from django.db import models
 
-
+User =get_user_model()
 class Restaurant(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="restaurants")
+
+
+    def __str__(self):
+        return self.name
--- backend/restaurant_review/views.py
+++ backend/restaurant_review/views.py
@@ -1,5 +1,9 @@
 # Create your views here.
+
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+
 from restaurant_review.models import Restaurant
 from restaurant_review.serializers import RestaurantReviewSerializer
 
@@ -8,6 +12,14 @@ class CreateRestaurantReviewAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         restaurant_id = self.kwargs['restaurant_id']
-        restaurant = Restaurant.objects.get(id=restaurant_id)
+        try:
+            # Fetch the restaurant instance
+            restaurant = Restaurant.objects.get(id=restaurant_id)
+        except Restaurant.DoesNotExist:
+            # Handle the case when the restaurant doesn't exist
+            return Response(
+                {"detail": "Restaurant not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
         user = self.request.user
         serializer.save(restaurant=restaurant, user=user)
