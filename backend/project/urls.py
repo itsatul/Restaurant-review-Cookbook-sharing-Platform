bf33fb564adeb8a2060777906d093bc4dbeec74a
Tiara <ht.nguyenthi@gmail.com>
2025-01-14 11:49:33 +0100

    commit all changes up to this point

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from project import settings
 
 urlpatterns = [
     path('backend/admin/', admin.site.urls),
+    path('backend/api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('backend/api/auth/token/refresh/', TokenRefreshView.as_view(), name='retrieve-refreshed-token'),
+    path('backend/api/auth/token/verify/', TokenVerifyView.as_view(), name='verify-token'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-//import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 import {Provider} from "react-redux";
 import store from "./store/index.js"
+import GlobalStyle from "./styles/globalStyles.js";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById("root")).render(
-    // <StrictMode>
-    <Provider store={store}>
-        <App/>
-    </Provider>
-    // </StrictMode>
+    <StrictMode>
+        <GlobalStyle>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </GlobalStyle>
+    </StrictMode>
 );
