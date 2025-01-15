f8ce4e70de4ce546bcedcbffdb4b32ce73f6fe85
Kevin <malieakkalkevin@gmail.com>
2025-01-15 19:57:15 +0100

    nginx setup revoked and cors  origins all true

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -27,6 +27,8 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173',
                         'https://luna-project-batch30.propulsion-learn.ch']
 
