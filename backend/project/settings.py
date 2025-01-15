b0851c1431d0cc0549d79b3d0752aa9546b75b75
Jon_Work_Linux <jon.garmilla.work@gmail.com>
2025-01-15 17:57:19 +0100

    add registration and password-reset, email verification

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -27,7 +27,8 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173','http://localhost:5173', 'https://luna-project-batch30.propulsion-learn.ch']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173', 'http://localhost:5173',
+                        'https://luna-project-batch30.propulsion-learn.ch']
 
 CSRF_TRUSTED_ORIGINS = ['https://luna-project-batch30.propulsion-learn.ch']
 # Application definition
