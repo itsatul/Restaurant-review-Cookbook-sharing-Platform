e5a91c214951a6556f241851bbfb68f2b4968377
Kevin <malieakkalkevin@gmail.com>
2025-01-17 12:24:18 +0100

    patch in user profile decoded

--- backend/user/views.py
+++ backend/user/views.py
@@ -11,6 +11,7 @@ from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveU
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from user.models import User
 from user.serializers import UserSerializer, UserprofileSerializer
 
